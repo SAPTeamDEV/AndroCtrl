2fe32b7cc3d1b7e7bef3758211f32b7c792d6085
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-02-07 19:07:42 +0330

    set program to discover connect services

--- MulticastDNSDiscovery/Program.cs
+++ MulticastDNSDiscovery/Program.cs
@@ -1,9 +1,11 @@
-﻿using Makaretu.Dns;
+﻿using System;
+
+using Makaretu.Dns;
 
 var mdns = new MulticastService();
 var sd = new ServiceDiscovery(mdns);
 
-mdns.NetworkInterfaceDiscovered += (s, e) =>
+/*mdns.NetworkInterfaceDiscovered += (s, e) =>
 {
     foreach (var nic in e.NetworkInterfaces)
     {
@@ -13,43 +15,42 @@ mdns.NetworkInterfaceDiscovered += (s, e) =>
     // Ask for the name of all services.
     sd.QueryAllServices();
 };
+*/
 
 sd.ServiceDiscovered += (s, serviceName) =>
 {
-    Console.WriteLine($"service '{serviceName}'");
+    if (serviceName == "_adb-tls-connect._tcp.local")
+    {
+        Console.WriteLine($"service '{serviceName}'");
 
-    // Ask for the name of instances of the service.
-    mdns.SendQuery(serviceName, type: DnsType.PTR);
+        // Ask for the name of instances of the service.
+        mdns.SendQuery(serviceName, type: DnsType.ANY);
+    }
 };
 
+/*
 sd.ServiceInstanceDiscovered += (s, e) =>
 {
-    Console.WriteLine($"service instance '{e.ServiceInstanceName}'");
+    if(e.ServiceInstanceName.Parent() == "_adb-tls-connect._tcp.local")
+    {
+        Console.WriteLine($"service instance '{e.ServiceInstanceName}' at {e.RemoteEndPoint.Address}:{e.RemoteEndPoint.Port}");
 
-    // Ask for the service instance details.
-    mdns.SendQuery(e.ServiceInstanceName, type: DnsType.SRV);
+        // Ask for the service instance details.
+        mdns.SendQuery(e.ServiceInstanceName, type: DnsType.SRV);
+    }
 };
+*/
 
 mdns.AnswerReceived += (s, e) =>
 {
-    // Is this an answer to a service instance details?
     var servers = e.Message.Answers.OfType<SRVRecord>();
     foreach (var server in servers)
     {
-        Console.WriteLine($"host '{server.Target}' for '{server.Name}'");
-
-        // Ask for the host IP addresses.
-        mdns.SendQuery(server.Target, type: DnsType.A);
-        mdns.SendQuery(server.Target, type: DnsType.AAAA);
+        if (server.Name.Parent() == "_adb-tls-connect._tcp.local")
+        {
+            Console.WriteLine($"host '{server.Target}' for '{server.Name}:{server.Port}'");
+        }
     }
-
-    // Is this an answer to host addresses?
-    var addresses = e.Message.Answers.OfType<AddressRecord>();
-    foreach (var address in addresses)
-    {
-        Console.WriteLine($"host '{address.Name}' at {address.Address}");
-    }
-
 };
 
 try
