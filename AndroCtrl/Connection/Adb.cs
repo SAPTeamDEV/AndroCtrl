2e034e40832e6a2899de36ad97b4f89c3bf4a82f
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-02-07 12:00:33 +0330

    Add Adb class

---
+++ AndroCtrl/Connection/Adb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+using AndroCtrl.Protocols.AndroidDebugBridge;
+
+namespace AndroCtrl.Connection;
+
+internal class Adb
+{
+    internal static AdbClient Client { get; private set; }
+    internal static AdbServer Server { get; private set; }
+
+    static string serverPath = @"bin\adb.exe";
+    static string localServer = @"C:\Windows\adb\adb.exe";
+
+    internal static List<DeviceData> Devices { get; private set; } = new List<DeviceData>();
+    internal static List<DeviceData> LiveDevices => Client.GetDevices();
+
+    static Adb()
+    {
+        Client = new AdbClient();
+        Server = new AdbServer(Client, Factories.AdbCommandLineClientFactory);
+        Server.StartServer(localServer, true);
+    }
+}
