17bc1fbf2e5e00b5e82b30bf75a24613e9a2e420
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-02-08 18:44:54 +0330

    Add Service Browser feature

---
+++ MulticastDNSDiscovery/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDNSDiscovery
+{
+    internal class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "browser")
+            {
+                mDnsServiceBrowser.Main(new string[1] { args[1] });
+            }
+            else
+            {
+                mDnsDiscovery.Main(Array.Empty<string>());
+            }
+        }
+    }
+}
