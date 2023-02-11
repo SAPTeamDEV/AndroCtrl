468112d0fd1a6f2117b08e10f89b9cadc2d4bfc4
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-02-11 18:35:58 +0330

    Add ReconnectionService class

---
+++ AndroCtrl/Services/EventArgs/ReconnectServiceEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroCtrl.Services.EventArgs
+{
+    public class ReconnectServiceEventArgs : IServiceEventArgs
+    {
+        public DnsEndPoint EndPoint { get; }
+        public int Delay { get; }
+
+        public ReconnectServiceEventArgs(DnsEndPoint ep, int delay)
+        {
+            EndPoint = ep;
+            Delay = delay;
+        }
+    }
+}
---
+++ AndroCtrl/Services/ReconnectionService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using AndroCtrl.Connection;
+using AndroCtrl.Services.EventArgs;
+
+namespace AndroCtrl.Services
+{
+    public class ReconnectionService : Service, IService
+    {
+        public ReconnectionService(DnsEndPoint ep, int delay)
+        {
+            runEventArgs = new ReconnectServiceEventArgs(delay);
+        }
+
+        protected override void Finish(object? sender, IServiceEventArgs e)
+        {
+            
+        }
+
+        protected override async void Run(object? sender, IServiceEventArgs e)
+        {
+            Status = ServiceStatus.Running;
+            ReconnectServiceEventArgs args = e;
+            DnsEndPoint ep = args.EndPoint;
+            int delay = args.Delay;
+
+            while (Status == ServiceStatus.Running)
+            {
+                await Task.Delay(delay);
+                try
+                {
+                    Adb.Client.Connect(ep);
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
